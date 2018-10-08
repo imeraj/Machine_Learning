6a1146f2340a7ab0d9f5855560d62acb7b49933c
Meraj <meraj.enigma@gmail.com>
2018-10-08 11:51:45 +0900

    iris-data-analysis: cross validation for model evaluation applied
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- MLPython/iris-data-analysis/main.py
+++ MLPython/iris-data-analysis/main.py
@@ -1,5 +1,8 @@
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import cross_val_score
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sb
@@ -71,4 +74,27 @@ for repetition in range(1000):
 
 print(model_accuracies)
 plt.hist(model_accuracies)
+plt.show()
+
+
+def plot_cv(cv, features, labels):
+    masks = []
+    for train, test in cv.split(features, labels):
+        mask = np.zeros(len(labels), dtype=bool)
+        mask[test] = 1
+        masks.append(mask)
+
+    plt.figure(figsize=(15, 15))
+    plt.imshow(masks, interpolation='none', cmap='gray_r')
+    plt.ylabel('Fold')
+    plt.xlabel('Row #')
+    plt.show()
+
+
+plot_cv(StratifiedKFold(n_splits=10), all_inputs, all_labels)
+
+random_forest_classifier = RandomForestClassifier(n_estimators=4)
+cv_scores = cross_val_score(random_forest_classifier, all_inputs, all_labels, cv=10)
+plt.hist(cv_scores)
+plt.title('Average score: {}'.format(np.mean(cv_scores)))
 plt.show()
