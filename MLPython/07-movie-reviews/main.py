c15b14e6176250eb391d2fc182f5655841ce7b4d
Meraj <meraj.enigma@gmail.com>
2018-10-10 20:15:32 +0900

    movie-reviews added with LogisticRegression
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

---
+++ MLPython/07-movie-reviews/main.py
@@ -0,0 +1,52 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import load_files
+from sklearn.model_selection import GridSearchCV
+import numpy as np
+
+reviews_train = load_files("aclImdb/train/")
+text_train, y_train = reviews_train.data, reviews_train.target
+
+print("type of text_train: {}".format(type(text_train)))
+print("length of text_train: {}".format(len(text_train)))
+print("text_train[1]:\n{}".format(text_train[1]))
+
+text_train = [doc.replace(b"<br />", b" ") for doc in text_train]
+print("Samples per class (training): {}".format(np.bincount(y_train)))
+
+reviews_test = load_files("aclImdb/test/")
+text_test, y_test = reviews_test.data, reviews_test.target
+
+print("Number of documents in test data: {}".format(len(text_test)))
+print("Samples per class (test): {}".format(np.bincount(y_test)))
+text_test = [doc.replace(b"<br />", b" ") for doc in text_test]
+
+vect = CountVectorizer(min_df=5)
+X_train = vect.fit(text_train).transform(text_train)
+X_test = vect.transform(text_test)
+
+print("Vocabulary size: {}".format(len(vect.vocabulary_)))
+# print("Vocabulary content:\n {}".format(vect.vocabulary_))
+print("X_train:\n{}".format(repr(X_train)))
+
+feature_names = vect.get_feature_names()
+print("Number of features: {}".format(len(feature_names)))
+print("First 20 features:\n{}".format(feature_names[:20]))
+print("Features 20010 to 20030:\n{}".format(feature_names[20010:20030]))
+print("Every 2000th feature:\n{}".format(feature_names[::2000]))
+
+scores = cross_val_score(LogisticRegression(), X_train, y_train, cv=5)
+print("Mean cross-validation accuracy: {:.2f}".format(np.mean(scores)))
+
+param_grid = {'C': [0.001, 0.01, 0.1, 1, 10]}
+grid = GridSearchCV(LogisticRegression(), param_grid, cv=5)
+grid.fit(X_train, y_train)
+print("Best cross-validation score: {:.2f}".format(grid.best_score_))
+print("Best parameters: ", grid.best_params_)
+print("Best estimator: ", grid.best_estimator_)
+
+lr = grid.best_estimator_
+lr.fit(X_train, y_train)
+lr.predict(X_test)
+print("Score: {:.2f}".format(lr.score(X_test, y_test)))
