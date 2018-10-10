ca1a3fccab6e3790b96c7a64493746541a5d6d32
Meraj <meraj.enigma@gmail.com>
2018-10-10 22:03:58 +0900

    Added pos and neg review
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- MLPython/07-movie-reviews/main.py
+++ MLPython/07-movie-reviews/main.py
@@ -56,3 +56,35 @@ lr = grid.best_estimator_
 lr.fit(X_train, y_train)
 lr.predict(X_test)
 print("Score: {:.2f}".format(lr.score(X_test, y_test)))
+
+pos = ["I've seen this story before but my kids haven't. Boy with troubled past joins military, faces his past, falls in love and becomes a man. "
+       "The mentor this time is played perfectly by Kevin Costner; An ordinary man with common everyday problems who lives an extraordinary "
+       "conviction, to save lives. After losing his team he takes a teaching position training the next generation of heroes. The young troubled "
+       "recruit is played by Kutcher. While his scenes with the local love interest are a tad stiff and don't generate enough heat to melt butter, "
+       "he compliments Costner well. I never really understood Sela Ward as the neglected wife and felt she should of wanted Costner to quit out of "
+       "concern for his safety as opposed to her selfish needs. But her presence on screen is a pleasure. The two unaccredited stars of this movie "
+       "are the Coast Guard and the Sea. Both powerful forces which should not be taken for granted in real life or this movie. The movie has some "
+       "slow spots and could have used the wasted 15 minutes to strengthen the character relationships. But it still works. The rescue scenes are "
+       "intense and well filmed and edited to provide maximum impact. This movie earns the audience applause. And the applause of my two sons."]
+print("Pos prediction: {}". format(lr.predict(vect.transform(pos))))
+
+neg = ["David Bryce\'s comments nearby are exceptionally well written and informative as almost say everything "
+       "I feel about DARLING LILI. This massive musical is so peculiar and over blown, over produced and must have "
+       "caused ruptures at Paramount in 1970. It cost 22 million dollars! That is simply irresponsible. DARLING LILI "
+       "must have been greenlit from a board meeting that said \"hey we got that Pink Panther guy and that Sound Of Music gal... "
+       "lets get this too\" and handed over a blank cheque. The result is a hybrid of GIGI, ZEPPELIN, HALF A SIXPENCE, some MGM 40s "
+       "song and dance numbers of a style (daisies and boaters!) so hopelessly old fashioned as to be like musical porridge, and MATA HARI "
+       "dramatics. The production is colossal, lush, breathtaking to view, but the rest: the ridiculous romance, Julie looking befuddled, Hudson "
+       "already dead, the mistimed comedy, and the astoundingly boring songs deaden this spectacular film into being irritating. LILI is"
+       " like a twee 1940s mega musical with some vulgar bits to spice it up. STAR! released the year before sadly crashed and now is being "
+       "finally appreciated for the excellent film is genuinely is... and Andrews looks sublime, mature, especially in the last half hour......"
+       "but LILI is POPPINS and DOLLY frilly and I believe really killed off the mega musical binge of the 60s..... "
+       "and made Andrews look like Poppins again... which I believe was not Edwards intention. Paramount must have collectively fainted "
+       "when they saw this: and with another $20 million festering in CATCH 22, and $12 million in ON A CLEAR DAY and $25 million in PAINT YOUR WAGON...."
+       "they had a financial abyss of CLEOPATRA proportions with $77 million tied into 4 films with very uncertain futures. Maybe they should have asked seer "
+       "Daisy Gamble from ON A CLEAR DAY ......LILI was very popular on immediate first release in Australia and ran in 70mm cinemas for months but it failed "
+       "once out in the subs and the sticks and only ever surfaced after that on one night stands with ON A CLEAR DAY as a Sunday night double. Thank "
+       "god Paramount had their simple $1million (yes, ONE MILLION DOLLAR) film LOVE STORY and that $4 million dollar gangster pic THE GODFATHER "
+       "also ready to recover all the $77 million in just the next two years....for just $5m.... incredible!"]
+print("Neg prediction: {}". format(lr.predict(vect.transform(neg))))
+
